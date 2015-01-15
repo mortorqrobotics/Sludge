38be1c514552f539abe2801e0b7ecf5631e36d34
DaPasta <484902@gmail.com>
2015-01-14 22:28:12 -0800

    Adds basic libGDX project.

---
+++ desktop/src/org/team1515/sludge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.team1515.sludge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.team1515.sludge.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
